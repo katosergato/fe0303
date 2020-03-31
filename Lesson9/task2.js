5c31043e8a33475b80b5e4c93b49bbaebd5e0919
Boris Cherepakha <bcherepakha@gmail.com>
2020-03-31 19:35:16 +0300

    lesson 9 homework

---
+++ Lesson9/task2.js
@@ -0,0 +1,29 @@
+const company = {
+    sales: [{
+      name: 'John',
+      salary: 1000
+    }, {
+      name: 'Alice',
+      salary: 600
+    }],
+
+    development: {
+      sites: [{
+        name: 'Peter',
+        salary: 2000
+      }, {
+        name: 'Alex',
+        salary: 1800
+      }],
+
+      internals: [{
+        name: 'Jack',
+        salary: 1300
+      }]
+    }
+  };
+
+function sumSalaries(department) {
+}
+
+console.log( sumSalaries(company) ); // 6700
---
+++ Lesson9/task3.js
@@ -0,0 +1,9 @@
+let name = "John";
+
+function sayHi() {
+  alert("Hi, " + name);
+}
+
+name = "Pete";
+
+sayHi(); // что будет показано: "John" или "Pete"?
