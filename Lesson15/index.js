5392c9dec5f53ebcaad44d4499db441514a24edb
Boris Cherepakha <bcherepakha@gmail.com>
2020-04-24 13:20:07 +0300

    Lesson

--- Lesson15/index.js
+++ Lesson15/index.js
@@ -1,15 +1,29 @@
 initMenu();
 
 function initMenu() {
-    const navList = document.querySelector('.nav__list'),
+    const navEl = document.querySelector('.nav'),
+        navList = navEl.querySelector('.nav__list'),
         activeBorder = navList.querySelector('.nav__item-active'),
-        links = navList.querySelectorAll('.nav__link');
+        links = navList.querySelectorAll('.nav__link'),
+        navBurgerEl = document.querySelector('.nav__burger'),
+        isWidthLess700 = window.matchMedia("(max-width: 700px)");
 
-    setTimeout(() => setActiveLink( links[0] ), 600);
+    if (isWidthLess700.matches) {
+        navBurgerEl.addEventListener('click', toggleMenu);
+    } else {
+        setTimeout(() => setActiveLink( links[0] ), 600);
 
-    for (let i=0; i<links.length; i++) {
-        links[i].addEventListener('mouseenter', setHover);
-        links[i].addEventListener('mouseleave', removeHover);
+        for (let i=0; i<links.length; i++) {
+            links[i].addEventListener('mouseenter', setHover);
+            links[i].addEventListener('mouseleave', removeHover);
+            links[i].addEventListener('focus', setHover);
+            links[i].addEventListener('blur', removeHover);
+        }
+    }
+
+    function toggleMenu() {
+        navList.classList.toggle('nav__list--open');
+        navEl.classList.toggle('nav--open');
     }
 
     function setHover( event ) {
